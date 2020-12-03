a460f33e8e77331184ae02f3558be85bf018bbbe
kento <kozukata1993@gmail.com>
2020-12-03 23:43:47 +0900

    チュートリアルの6終わり

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -12,10 +12,24 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
   constructor(private heroService: HeroService, private messageService: MessageService) { }
+
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
+  add(name: string): void {
+    name = name.trim();
+    !name && console.log('name is false')
+    // if (!name) { return; }
+    this.heroService.addHero({ name } as Hero).subscribe(hero => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
+  }
 
   // getHeroes() はコンストラクターでも呼び出すことはできますが、これは最適な方法ではありません。
   // コンストラクターではプロパティ定義などの簡単な初期化のみを行い、それ以外は 何もするべきではありません。
